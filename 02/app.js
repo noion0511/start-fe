cf827cd31d399594a0959a6ed5d6259050674f04
noion0511 <noion0511@gmail.com>
2022-03-10 17:27:59 +0900

    2주차 실습 내용

--- 02/app.js
+++ 02/app.js
@@ -21,9 +21,13 @@ console.log(img);
 
 var arr = [1,2,3,4,5];
 console.log(arr);
+console.log(arr.length);
+for (var i=0; i<arr.length; i++) {
+    console.log(arr[i]);
+}
 
 console.log(1+1);
-console.log(name+'hello')
+console.log(name2+'hello')
 
 var me = {
     age: 11,
