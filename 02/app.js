0ac4788d80536c70efefaea605b599bb9774cd9b
noion0511 <noion0511@gmail.com>
2022-03-10 17:17:38 +0900

    2주차 강의

---
+++ 02/app.js
@@ -0,0 +1,35 @@
+/*
+    2주차 실습
+*/
+var name2 = 'huiju';
+console.log(name2);
+
+var isMan = 'true';
+var isMan2 = true;
+console.log(isMan);
+console.log(isMan2);
+
+var log = function(str) {
+    console.log(str);
+  };
+log('hello');
+
+var img = {};
+img.width = 100;
+img.height = 200;
+console.log(img);
+
+var arr = [1,2,3,4,5];
+console.log(arr);
+
+console.log(1+1);
+console.log(name+'hello')
+
+var me = {
+    age: 11,
+    name: 'huiju',
+    isMan: false,
+    sayHello: function () {
+        console.log('hello');
+    },
+};
---
+++ 02/node1.js
@@ -0,0 +1,2 @@
+var name = 'huiju';
+console.log(name);
